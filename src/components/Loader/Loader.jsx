5faee117dda020081d8485cf002824a46de36114
Cristi Manea <cristimanea26@gmail.com>
2023-01-25 12:52:34 +0200

    update loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { motion as m } from 'framer-motion';
-import { fadeIn } from '../Animations.jsx';
 import './Loader.css';
 
 const Loader = () => {
     return (
-        <m.div className='loader' variants={fadeIn} initial='hidden' animate='show'>
+        <div className='loader'>
             <div className='loader-inner'></div>
-        </m.div>
+        </div>
     )
 }
 
