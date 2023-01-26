0b2b01ce761fb3889edc8b550c840768c01a5921
Cristi Manea <cristimanea26@gmail.com>
2023-01-26 12:47:12 +0200

    update7

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import ReactGA from 'react-ga';
 import App from './App';
 import './index.css';
@@ -10,8 +10,8 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <MemoryRouter>
       <App />
-    </BrowserRouter>
+    </MemoryRouter>
   </React.StrictMode>,
 )
