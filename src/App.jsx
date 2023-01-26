397fdff164d0da6135c5e2cbf4647b2d4e31fe08
Cristi Manea <cristimanea26@gmail.com>
2023-01-26 12:13:24 +0200

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Loader from './components/Loader/Loader.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Home from './components/Home/Home.jsx';
 import Projects from './components/Projects/Projects.jsx';
 import Skills from './components/Skills/Skills.jsx';
 import Contact from './components/Contact/Contact.jsx';
-import ReactGA from 'react-ga';
 import './App.css';
 
 const App = () => {
-  ReactGA.initialize('G-BXR0LZ45ZF');
-  ReactGA.pageview(window.location.pathname + window.location.search);
-
+  const location = useLocation();
   const [loading, setLoading] = useState(false);
-
+  
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
@@ -26,15 +23,16 @@ const App = () => {
     <div className='App'>
       { loading
       ? (<Loader />)
-      : (<BrowserRouter>
+      : (<>
             <Navbar />
-            <Routes>
-                <Route path='/' element={<Home/>}/>
+            <Routes location={location} key={location.pathname}>
+                <Route path='/' exact element={<Home/>}/>
                 <Route path='/projects' element={<Projects/>}/>
                 <Route path='/skills' element={<Skills/>}/>
                 <Route path='/contact' element={<Contact/>}/>
             </Routes>
-        </BrowserRouter>)
+          </>
+        )
       }
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MemoryRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import App from './App';
 import './index.css';
 
+ReactGA.initialize('G-BXR0LZ45ZF');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
   </React.StrictMode>,
 )
